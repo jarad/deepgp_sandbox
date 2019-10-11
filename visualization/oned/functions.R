3e502be32a73e86dba3d1758a5901b7b39db3b4a
Jarad Niemi <niemi@iastate.edu>
2019-10-11 15:59:48 -0700

    Add hidden layer plots

--- visualization/oned/functions.R
+++ visualization/oned/functions.R
@@ -2,10 +2,10 @@ library("MASS")
 
 # Function to compute covariance matrix based on squared exponential
 # assuming scalar locations
-create_covariance_matrix <- function(x, sigma, lengthscale) {
+create_covariance_matrix <- function(x, sigma, lengthscale, nugget) {
   d <- length(x) # assumes scalar locations
   sigma2 <- sigma^2
-  m <- matrix(sigma2, nrow = d, ncol = d) 
+  m <- matrix(sigma2 + nugget, nrow = d, ncol = d) 
   for (i in 1:(d-1)) {
     for (j in 2:d) {
       m[i,j] <- m[j,i] <- sigma2*exp(-(x[i]-x[j])^2/lengthscale)
@@ -14,17 +14,18 @@ create_covariance_matrix <- function(x, sigma, lengthscale) {
   return(m)
 }
 
-rgp <- function(x, sigma, lengthscale) {
+rgp <- function(x, sigma, lengthscale, nugget) {
   n <- length(x)
-  K <- create_covariance_matrix(x, sigma, lengthscale)
+  K <- create_covariance_matrix(x, sigma, lengthscale, nugget)
   mvrnorm(1, rep(0,n), K)
 }
 
-rdeepgp <- function(x, sigma, lengthscale, n_layers = 0) {
-  f = rgp(x, sigma, lengthscale)
-  while (n_layers > 0) {
-    n_layers <- n_layers - 1
-    f <- rgp(f, sigma, lengthscale)
-  }
+rdeepgp <- function(x, sigma, lengthscale, nugget, n_layers) {
+  f = matrix(NA, nrow = length(x), ncol = n_layers + 2)
+  f[,n_layers + 2] = x
+  
+  for (i in (n_layers+1):1) 
+    f[,i] = rgp(f[,i+1], sigma[i], lengthscale[i], nugget[i])
+
   return(f)
 }
